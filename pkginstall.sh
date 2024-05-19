a1fd11484703ac1c6f0f580e984aea379478c188
Phạm Bình An <111893501+brianhuster@users.noreply.github.com>
2024-05-20 01:28:44 +0700

    Update pkginstall.sh

--- pkginstall.sh
+++ pkginstall.sh
@@ -22,5 +22,14 @@ rm -f packages.microsoft.gpg
 sudo apt install apt-transport-https
 sudo apt install code # or code-insiders
 
+#Jekyll
+sudo apt-get install ruby-full build-essential zlib1g-dev
+echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+gem install jekyll bundler
+
+# config touchpad
 sudo apt install touchpad-indicator
 # Then open touchpad-indicator to disable touchpad when mouse is plugged in
