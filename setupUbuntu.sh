8ed801b8a1f5a2e68a48fe35ea15fee3e4032fb4
Phạm Bình An <111893501+brianhuster@users.noreply.github.com>
2024-05-20 10:13:49 +0700

    Update setupUbuntu.sh

--- setupUbuntu.sh
+++ setupUbuntu.sh
@@ -8,6 +8,7 @@ echo | sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 sudo apt-get update
 sudo apt-get install -y ibus ibus-bamboo ibus-mozc --install-recommends
 ibus restart
+env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo', 'mozc']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo'), ('ibus', 'mozc')]"
 
 # install ollama and stablecode
 curl -fsSL https://ollama.com/install.sh | sh
