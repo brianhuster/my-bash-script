e585db0f5bea11d92cba73697c388d6d87fb8fc0
Phạm Bình An <111893501+brianhuster@users.noreply.github.com>
2024-05-21 16:44:33 +0700

    Update setupUbuntu.sh

--- setupUbuntu.sh
+++ setupUbuntu.sh
@@ -8,7 +8,7 @@ echo 'eval "$(gh copilot alias -- bash)"' >> ~/.bashrc
 
 # install programming tools
 sudo apt install build-essential
-sudo apt install git docker
+sudo apt install -y git docker
 sudo apt install -y mysql-server mongodb-org
 sudo apt install -y python3-pip python3-venv
 pip install -y jupyterlab sckit-learn
