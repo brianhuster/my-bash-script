39a10609c59eac8e4e0c18cc343a6055fbb6fa51
Phạm Bình An <111893501+brianhuster@users.noreply.github.com>
2024-05-31 00:46:29 +0700

    Update setupKbuntu.sh

--- setupKbuntu.sh
+++ setupKbuntu.sh
@@ -1,6 +1,15 @@
-# add repository for Linux graphic drivers
-sudo add-apt-repository ppa:oibaf/graphics-drivers
-sudo apt update && sudo apt upgrade -y
+# add repository for Intel graphic drivers
+wget -qO - https://repositories.intel.com/gpu/intel-graphics.key | \
+  sudo gpg --yes --dearmor --output /usr/share/keyrings/intel-graphics.gpg
+echo "deb [arch=amd64,i386 signed-by=/usr/share/keyrings/intel-graphics.gpg] https://repositories.intel.com/gpu/ubuntu jammy client" | \
+  sudo tee /etc/apt/sources.list.d/intel-gpu-jammy.list
+sudo apt update
+sudo apt install -y \
+  intel-opencl-icd intel-level-zero-gpu level-zero \
+  intel-media-va-driver-non-free libmfx1 libmfxgen1 libvpl2 \
+  libegl-mesa0 libegl1-mesa libegl1-mesa-dev libgbm1 libgl1-mesa-dev libgl1-mesa-dri \
+  libglapi-mesa libgles2-mesa-dev libglx-mesa0 libigdgmm12 libxatracker2 mesa-va-drivers \
+  mesa-vdpau-drivers mesa-vulkan-drivers va-driver-all vainfo hwinfo clinfo
 
 # install gh copilot
 sudo apt install gh && gh auth login
