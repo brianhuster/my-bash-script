e09469b301080735152275d71629f5f6a5dc5b50
Phạm Bình An <111893501+brianhuster@users.noreply.github.com>
2024-06-09 09:44:10 +0700

    Update setupKbuntu.sh

--- setupKbuntu.sh
+++ setupKbuntu.sh
@@ -56,6 +56,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
+npm install -g prettier
 
 #Install Vietnamese and Japanese input method
 echo | sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
