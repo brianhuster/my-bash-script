3d6cd30c21310be185e62829e6157b842a2e5ab2
Phạm Bình An <111893501+brianhuster@users.noreply.github.com>
2024-08-10 07:19:15 +0700

    Update setupKbuntu.sh

--- setupKbuntu.sh
+++ setupKbuntu.sh
@@ -44,7 +44,16 @@ sudo apt install build-essential
 sudo apt install -y git docker
 sudo apt install -y mysql-server mongodb-org
 sudo apt install -y python3-pip python3-venv
-pip install -y jupyterlab sckit-learn
+pip install -y jupyterlab sckit-learn 
+
+# Mongodb
+sudo apt-get install gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 
 # install Hack Nerd Font
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
