34c664b5cd47765286f6d370f16903d30a410c5e
Phạm Bình An <111893501+brianhuster@users.noreply.github.com>
2024-09-01 22:28:50 +0700

    Update setupKbuntu.sh

--- setupKbuntu.sh
+++ setupKbuntu.sh
@@ -111,14 +111,7 @@ flatpak install flathub com.brave.Browser
 flatpak install flathub com.usebottles.bottles
 
 #VScode
-sudo apt-get install -y wget gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
-sudo apt install -y apt-transport-https
-sudo apt update
-sudo apt install -y code # or code-insiders
+sudo snap install code
 
 #Jekyll
 sudo apt-get install ruby-full zlib1g-dev
