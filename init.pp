35fdf2b5319c61e8393bd2197c4d610bf9e6955a
Phạm Bình An <111893501+brianhuster@users.noreply.github.com>
2026-09-03 17:56:19 +0700

    Remove and update package names in init.pp

--- init.pp
+++ init.pp
@@ -1,25 +1,23 @@
 $packages = [
 	'curl',
 	'lm-sensors',
-	'ubuntu-restricted-extras',
 	'fish',
-	'build-essential',
 	'git',
 	'python3-pip',
 	'python3-venv',
 	'pipx',
 	'jupyter-notebook',
-	'flatpak',
-	'plasma-discover-backend-flatpak',
 	'kdenlive', 'obs-studio',
-	'ttf-mscorefonts-installer',
 	'gnupg',
 	"fcitx5", "fcitx5-unikey", "fcitx5-mozc", "fcitx5-pinyin",
 	"mysql-server",
 	"nodejs", "npm",
-	"golang-go",
+	"golang",
+	"gcc", "g++", "make",
 	"neovim",
-	"gh"
+	"gh",
+	'flatpak',
+	'plasma-discover-backend-flatpak',
 ]
 
 package { $packages:
