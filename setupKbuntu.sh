dddfc0605d47ed513be5bff5f10bf7e6fb1b4e4a
Phạm Bình An <111893501+brianhuster@users.noreply.github.com>
2026-09-03 18:00:22 +0700

    Refactor setup script by removing unnecessary installations
    Removed installation of various tools and packages including gh, input methods, wine, and others. Added puppet installation and updated GRUB configuration.

--- setupKbuntu.sh
+++ setupKbuntu.sh
@@ -1,3 +1,6 @@
+sudo apt install puppet
+sudo puppet apply -v init.pp
+
 # add repository for Intel graphic drivers
 wget -qO - https://repositories.intel.com/gpu/intel-graphics.key | \
   sudo gpg --yes --dearmor --output /usr/share/keyrings/intel-graphics.gpg
@@ -11,8 +14,6 @@ sudo apt install -y \
   libglapi-mesa libgles2-mesa-dev libglx-mesa0 libigdgmm12 libxatracker2 mesa-va-drivers \
   mesa-vdpau-drivers mesa-vulkan-drivers va-driver-all vainfo hwinfo clinfo
 
-sudo apt install curl
-sudo apt install lm-sensors
 sudo apt install ubuntu-restricted-extras
 sudo usermod -a -G dialout $USER
 sudo usermod -aG plugdev $LOGNAME
@@ -26,28 +27,6 @@ echo "Modifying GRUB configuration..."
 sudo sed -i 's/GRUB_CMDLINE_LINUX="/&acpi_osi=\Windows2022 /' $GRUB_FILE
 echo "Updating GRUB..."
 sudo update-grub
-
-
-# install gh
-sudo apt install -y gh && gh auth login
-gh extension install github/gh-copilot
-gh extension upgrade gh-copilot
-echo 'eval "$(gh copilot alias -- bash)"' >> ~/.bashrc
-
-sudo apt-get install -y xclip
-sudo apt-get install -y xsel
-sudo apt install -y fish
-chsh -s $(which fish)
-
-# install programming tools
-sudo apt install build-essential
-sudo apt install -y git docker
-sudo apt install -y mysql-server mongodb-org
-sudo apt install -y python3-pip python3-venv
-sudo apt install pipx
-sudo apt install jupyter-notebook
-sudo snap install arduino-cli
-sudo apt-get install android-sdk-platform-tools-common
 
 # Mongodb
 sudo apt-get install gnupg curl
@@ -69,17 +48,6 @@ sudo apt install -y coccoc-browser-stable
 
 # Go
 sudo snap install go --classic
-go install github.com/jesseduffield/lazygit@latest
-
-#Install Vietnamese and Japanese input method
-echo | sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
-sudo apt-get update
-sudo apt-get install -y ibus ibus-bamboo ibus-mozc --install-recommends
-ibus restart
-env DCONF_PROFILE=ibus dconf write /desktop/ibus/general/preload-engines "['BambooUs', 'Bamboo', 'mozc-jp']" && gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('ibus', 'Bamboo'), ('ibus', 'mozc-jp')]"
-
-# fctx5
-sudo apt install fcitx5-bamboo fcitx5-mozc fcitx5-pinyin --install-recommends
 
 # install neovim
 sudo snap install nvim --classic --edge
@@ -96,13 +64,6 @@ sudo apt install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.usebottles.bottles
 
-# install wine
-sudo apt install wine32 wine64 winetricks
-winetricks corefonts
-sudo mkdir -p ~/opt/wine-stable/share/wine/gecko
-sudo wget -O ~/opt/wine-stable/share/wine/gecko/wine-gecko-2.47.1-x86.msi https://dl.winehq.org/wine/wine-gecko/2.47.1/wine-gecko-2.47.1-x86.msi
-sudo wget -O ~/opt/wine-stable/share/wine/gecko/wine-gecko-2.47.1-x86_64.msi https://dl.winehq.org/wine/wine-gecko/2.47.1/wine-gecko-2.47.1-x86_64.msi
-
 # Install dotfiles
 git clone --depth 1 https://github.com/brianhuster/dotfiles
 cd dotfiles
@@ -111,9 +72,6 @@ cd -
 
 # install other apps
 sudo snap install spotify
-sudo apt install xdotool
-sudo apt install kdenlive
-sudo apt install obs-studio
 
 # install onlyofficex
 sudo apt-get install ttf-mscorefonts-installer
